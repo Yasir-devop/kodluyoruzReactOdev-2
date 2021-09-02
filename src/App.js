7830662d6e5662dc1c1c904b130b85db2124f63b
Yasir-devop <yasirku4@github.com>
2021-09-02 13:12:56 +0300

    odev2 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import Input from "./components/Input";
+import List from "./components/List";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { desc: "Learn Javascript", toggled: true },
+    { desc: "Learn React", toggled: false },
+  ]);
+  
+  const [filter, setFilter] = useState("All");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="title"> todos </h1>
+      <div className="todoPanel">
+        <Input todos={todos} setTodos={setTodos} />
+        <List todos={todos} setTodos={setTodos} filter={filter} />
+        <Footer
+          todos={todos}
+          setTodos={setTodos}
+          filter={filter}
+          setFilter={setFilter}
+        />
+      </div>
     </div>
   );
 }
